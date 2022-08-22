ef289c1aedf8e783703ed890c89b43ad51b804c3
andrematheus2 <am_arao@hotmail.com>
2022-08-22 14:40:43 -0300

    Classe de novas empresas, criando nova empresa e simulando um banco de dados com o metodo adiciona

---
+++ gerenciador/src/main/java/com/br/andre/gerenciador/servlet/Banco.java
@@ -0,0 +1,20 @@
+package com.br.andre.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+	private static List<Empresa> lista = new ArrayList<>();
+	
+	public void adiciona(Empresa empresa) {
+
+		Banco.lista.add(empresa);
+		
+	}
+	
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+}
