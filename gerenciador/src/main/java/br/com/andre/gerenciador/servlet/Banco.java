7d3959298c9a987c8a81080eb52569504ecb614f
andrematheus2 <am_arao@hotmail.com>
2022-08-23 14:49:38 -0300

    remove empresa

--- gerenciador/src/main/java/br/com/andre/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/andre/gerenciador/servlet/Banco.java
@@ -1,16 +1,21 @@
 package br.com.andre.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();	
+	private static Integer chaveSequencial = 1;
 	
     static {
         Empresa empresa = new Empresa();
+        empresa.setId(chaveSequencial++);
         empresa.setNome("Alura");
         Empresa empresa2 = new Empresa();
+        empresa2.setId(chaveSequencial++);
+
         empresa2.setNome("Caelum");
         lista.add(empresa);
         lista.add(empresa2);
@@ -19,7 +24,7 @@ public class Banco {
     }
 	
 	public void adiciona(Empresa empresa) {
-
+        empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 		
 	}
@@ -28,4 +33,15 @@ public class Banco {
 		return Banco.lista;
 	}
 
+	public void removeEmpresa(Integer id) {
+		   Iterator<Empresa> it = lista.iterator();
+
+		    while(it.hasNext()) { 
+		        Empresa emp = it.next();
+
+		        if(emp.getId() == id ) {
+		            it.remove();
+		        }
+		    }
+	 }
 }
---
+++ gerenciador/src/main/java/br/com/andre/gerenciador/servlet/removeEmpresaServlet.java
@@ -0,0 +1,27 @@
+package br.com.andre.gerenciador.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/removeEmpresa")
+public class removeEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		Banco banco = new Banco();
+		banco.removeEmpresa(id);
+		
+		response.sendRedirect("listaEmpresas");
+		
+	}
+
+}
