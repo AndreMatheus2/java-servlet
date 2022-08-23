11df7df43c9690c8838c069946e6bb0b01b6381a
andrematheus2 <am_arao@hotmail.com>
2022-08-23 10:19:27 -0300

    teste

--- gerenciador/src/main/java/br/com/andre/gerenciador/servlet/ListaEmpresasServlet.java
+++ gerenciador/src/main/java/br/com/andre/gerenciador/servlet/ListaEmpresasServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/listaEmrpresas")
+@WebServlet("/listaEmpresas")
 public class ListaEmpresasServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
